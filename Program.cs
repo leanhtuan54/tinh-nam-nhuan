443766f4f735d0e1654fba68068eed386e608975
leminhtri1511 <trile20021511@gmail.com>
2024-08-08 14:17:45 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TinhNamNhuan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+
+            Console.Write("Enter a year: ");
+            year = int.Parse(Console.ReadLine());
+
+            if (year % 4 == 0)
+            {
+                if (year % 100 != 0)
+                {
+                    Console.WriteLine($"{year} is a leap year");
+                }
+                else if (year % 100 == 0 && year % 400 != 0)
+                {
+                    Console.WriteLine($"{year} is not leap year");
+                }
+                else if (year % 100 == 0 && year % 400 == 0)
+                {
+                    Console.WriteLine($"{year} is a leap year");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not leap year");
+            }
+        }
+    }
+}
